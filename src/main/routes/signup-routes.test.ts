40567f7448459af732425fd0c101df4c25ad7ab9
alanwcg <alancintra7@gmail.com>
2022-01-22 10:55:28 -0300

    test: ensure SignUp route return an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  it('should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Alan',
+        email: 'alancintra@gmail.com',
+        password: '123456',
+        passwordConfirmation: '123456'
+      })
+      .expect(200)
+  })
+})
