240733e4eb43ec0c91d461e16d0ea0ae4f2280d1
alanwcg <alancintra7@gmail.com>
2022-02-08 08:17:48 -0300

    refactor: improving file names

--- src/main/factories/login/login-validation-factory.spec.ts
+++ src/main/factories/login/login-validation-factory.spec.ts
@@ -1,11 +1,10 @@
 import {
   ValidationComposite,
   RequiredFieldValidation,
-  CompareFieldsValidation,
   EmailValidation
 } from '../../../presentation/helpers/validators'
 import { Validation, EmailValidator } from '../../../presentation/protocols'
-import { makeSignUpValidation } from './signup-validation'
+import { makeLoginValidation } from './login-validation-factory'
 
 jest.mock('../../../presentation/helpers/validators/validation-composite')
 
@@ -19,15 +18,14 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
-describe('SignUpValidation Factory', () => {
+describe('LoginValidation Factory', () => {
   it('should create ValidationComposite with all validations', () => {
-    makeSignUpValidation()
+    makeLoginValidation()
 
     const validations: Validation[] = []
-    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    for (const field of ['email', 'password']) {
       validations.push(new RequiredFieldValidation(field))
     }
-    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     validations.push(new EmailValidation('email', makeEmailValidator()))
 
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
