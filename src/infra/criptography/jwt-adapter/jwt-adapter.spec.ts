ff453cece2cb22717470eedbbe7e18c781168b7e
alanwcg <alancintra7@gmail.com>
2022-02-18 18:53:58 -0300

    test: ensure JwtAdapter returns a decrypted value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -56,5 +56,13 @@ describe('Jwt Adapter', () => {
 
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
+
+    it('should return a value on verify success', async () => {
+      const sut = makeSut()
+
+      const value = await sut.decrypt('any_token')
+
+      expect(value).toBe('any_value')
+    })
   })
 })
